ed0230f02eb1cecd2f8c1283acf192312c9dc4f2
Yulya <printer3d.smr>
2023-06-30 22:03:58 +0400

    пр5

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,8 @@ const popupImage = document.querySelector('.popup-image');
 const closeImg = popupImage.querySelector('.popup-image__button-close');
 const popupInputName = popup.querySelector('.popup__input_data_name');
 const popupInputInfo = popup.querySelector('.popup__input_info_name');
-const popupInputDataNameAdd = popupAdd.querySelector('.popup__input_edit_image-name');
-const popupInputUrlNameAdd = popupAdd.querySelector('.popup__input_edit_image-url');
+const popupInputDataNameAdd = popupAdd.querySelector('.popup-add__input_data_name');
+const popupInputUrlNameAdd = popupAdd.querySelector('.popup-add__input_url');
 const profileTitle = document.querySelector('.profile__title');
 const profileSubtitle = document.querySelector('.profile__subtitle');
 
